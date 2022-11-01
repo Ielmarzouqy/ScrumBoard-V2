14f2aa7f3fcff6ebe2c1a8e8b6829d54328b8ee4
Ielmarzouqy <elmarzouqyimane@gmail.com>
2022-11-01 17:45:41 +0100

    New modal

--- scripts.php
+++ scripts.php
@@ -1,130 +1,92 @@
- <?php 
-// include database file
-include './include/database.php';
+<?php
+    //INCLUDE DATABASE FILE
+    include('database.php');
+    //SESSSION IS A WAY TO STORE DATA TO BE USED ACROSS MULTIPLE PAGES
+    session_start();
 
-if(isset($_POST['save']))        saveTask();
-if(isset($_POST['update']))      updateTask();
-if(isset($_POST['delete']))      deleteTask();
-
-
-function getTasks($status){
-    global $conn;
-    global $tasks;
-
-    $sql = "SELECT tasks.id,tasks.title, status.name as status, types.name as type, priorities.name as priority, tasks.task_datetime as date, tasks.description  
-            FROM `tasks` 
-                 inner join status on tasks.status_id = status.id
-                 inner join types on tasks.type_id = types.id
-                 inner join priorities on tasks.priority_id = priorities.id";
-    $result = mysqli_query($conn, $sql);
-
-    // $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    // print_r($tasks) ;
-
-   
-    if(mysqli_num_rows($result) > 0){
-         while($row = mysqli_fetch_assoc($result)){
-             if ($row["status"] == 'to do'){
-                echo ('<button class="task p-3 border-0 text-white bg-none mt-2 col-12 d-flex">
-                <div class="">
-                <i class="bi bi-question-circle-fill text-success me-2"></i></div>
-                </div>
-                <div class="text-start">
-                    <div class="text-white ">'.$row['title'].'</div>
-                    <div class="">
-                        <div class="">'.$row['date'].'</div>
-                        <div class="" title=" and details in the main/primary description of a tas.">'.$row['description'].'...</div>
-                        </div>
-                        <div class="mt-2 ms-4">
-                            <span class="btn">'.$row['priority'].'</span>
-                            <span class="btn2"></span> '.$row['type'].'</div>
-                            </div>
-                            <div class = "">
-                            <button onclick = "" type="button" class="btn4 me-3 mb-3 float-right border"  data-toggle="modal" data-target="#modal-task" >Edit</button>
-                            <button onclick = "" type="button" class="btn3 me-3 float-right border" data-dismiss="modal">Delete</button>
-                            </div>
-                        </div>
-                    </button>');
-            
-             }else {
-                if ($row["status"] == 'in progress'){
-                    echo ('<button class="task p-3 border-0 text-white bg-none mt-2 col-12 d-flex">
-                    <div class="">
-                    <i class="spinner-border  spinner-border-sm  text-success me-2"></i> 
-                    </div>
-                    <div class="text-start">
-                        <div class="text-white ">'.$row['title'].'</div>
-                        <div class="">
-                            <div class="">'.$row['date'].'</div>
-                            <div class="" title=" and details in the main/primary description of a tas.">'.$row['description'].'...</div>
-                            </div>
-                            <div class="mt-2 ms-4">
-                                <span class="btn">'.$row['priority'].'</span>
-                                <span class="btn2"></span> '.$row['type'].'</div>
-                                </div>
-                                <div class = "">
-                                <button onclick = "" type="button" class="btn4 me-3 mb-3 float-right border"  data-toggle="modal" data-target="#modal-task" >Edit</button>
-                                <button onclick = "" type="button" class="btn3 me-3 float-right border" data-dismiss="modal">Delete</button>
-                                </div>
-                            </div>
-                        </button>');
-                
-                 }else{
-                    if ($row["status"] == 'done'){
-                        echo ('<button class="task p-3 border-0 text-white bg-none mt-2 col-12 d-flex">
-                        <div class="">
-                        <i class="bi bi-check-circle  text-success me-2"></i> 
-                        </div>
-                        <div class="text-start">
-                            <div class="text-white ">'.$row['title'].'</div>
-                            <div class="">
-                                <div class="">'.$row['date'].'</div>
-                                <div class="" title=" and details in the main/primary description of a tas.">'.$row['description'].'...</div>
-                                </div>
-                                <div class="mt-2 ms-4">
-                                    <span class="btn">'.$row['priority'].'</span>
-                                    <span class="btn2"></span> '.$row['type'].'</div>
-                                    </div>
-                                    <div class = "">
-                                    <button onclick = "" type="button" class="btn4 me-3 mb-3 float-right border"  data-toggle="modal" data-target="#modal-task" >Edit</button>
-                                    <button onclick = "" type="button" class="btn3 me-3 float-right border" data-dismiss="modal">Delete</button>
-                                    </div>
-                                </div>
-                            </button>');
-                    
-                     }
-                 }
-             }
-        }
-    }
-}
- function saveTask(){
-    global $conn;
-
-    $title = $_POST['title'];
-    $type = $_POST['type'];
-    $priority = $_POST['priority'];
-    $status = $_POST['status'];
-    $date = $_POST['date'];
-    $description = $_POST['description'];
+    //ROUTING
+    if(isset($_POST['save']))        saveTask();
+    if(isset($_POST['update']))      updateTask();
+    if(isset($_POST['delete']))      deleteTask();
     
-    $sql = "INSERT INTO tasks( title, type_id, priority_id, status_id, task_datetime, description)
-    VALUES ('$title', '$type', '$priority', '$status', '$date', '$description');";
 
-    $results = mysqli_query($conn, $sql);
+    function getTasks($status)
+    {
+        global $conn;
 
-    if($results){
-        echo "data inserted";
-    } else {
-        echo 'data isnot inserted';
+        $sql = "SELECT tasks.id,tasks.title, status.name as status, types.name as type, priorities.name as priority, tasks.task_datetime as date, tasks.description  
+        FROM `tasks` 
+             inner join status on tasks.status_id = status.id
+             inner join types on tasks.type_id = types.id
+             inner join priorities on tasks.priority_id = priorities.id";
+        $result = mysqli_query($conn, $sql);
+        //CODE HERE
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                 //SQL SELECT
+                // echo "Fetch all tasks";
+                if ($row['status'] == $status){
+                   echo '
+                        <button id ="id_task" class="d-flex" onclick ="edit_tasks(`'.$row['title'].'`, `'.$row['date'].'`, `'.$row['description'].'`, `'.$row['priority'].'`, `'.$row['type'].'`, `'.$row['status'].'`)" data-bs-toggle="modal" data-bs-target="#modal-task">
+                            <span class = "icon">
+                                <i class="bi bi-question-circle-fill text-green"></i>
+                            </span>
+                            <div class=" task-body align-center text-start">
+                                <div class ="title mb-2"><h5>'.$row['title'].'</h5></div>
+                                <div class = "date mb-2">'.$row['date'].'</div>
+                                <div class ="description mb-2" >'.$row['description'].'</div>
+                                <span class = "btn btn-danger priority">'.$row['priority'].'</span>
+                                <span class = "btn btn-info type">'.$row['type'].'</span>
+                            </div>
+                        </button>
+
+                        ';
+                }
+               
+            }
+       
     }
-} 
-
-function deleteTask(){
-    // TODO 
 }
+    function saveTask()
+    {
+        global $conn;
+        $title = $_POST['title'];
+        $type = $_POST['type'];
+        $priority = $_POST['priority'];
+        $status = $_POST['status'];
+        $date = $_POST['date'];
+        $description = $_POST['description'];
 
-function updateTask(){
-    // TODO
-}
+        //CODE HERE
+        //SQL INSERT
+        $sql = "INSERT INTO tasks( title, type_id, priority_id, status_id, task_datetime, description)
+                VALUES ('$title', '$type', '$priority', '$status', '$date', '$description');";
+
+        $results = mysqli_query($conn, $sql);
+
+        $_SESSION['message'] = "Task has been added successfully !";
+		header('location: index.php');
+    }
+
+    function updateTask()
+    {
+       
+        //CODE HERE
+
+        //SQL UPDATE
+        $_SESSION['message'] = "Task has been updated successfully !";
+		header('location: index.php');
+    }
+
+    function deleteTask()
+    {
+        //CODE HERE
+        //SQL DELETE
+        $_SESSION['message'] = "Task has been deleted successfully !";
+		header('location: index.php');
+    }
+
+    ?>
+
+
 
